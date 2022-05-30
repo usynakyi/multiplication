d8ab5e1fd5d862de734294a35852c4d1fe209a75
Volodymyr Usynskyi <uvs2003@ukr.net>
2022-05-30 16:47:37 +0300

    test 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int sizeOfArray = random.nextInt(100);
+
+        do{
+            sizeOfArray = random.nextInt(100);
+        }while (sizeOfArray % 2 == 1);
+
+        int[] myArray = new int[sizeOfArray];
+
+        for (int i = 0; i < sizeOfArray; i++) {
+
+            int myVariable = random.nextInt(100);
+
+            do{
+                myVariable = random.nextInt(100);
+            }while (myVariable % 2 == 1);
+
+            myArray[i] = myVariable;
+        }
+
+        int halfOfArray = sizeOfArray / 2;
+        Arrays.sort(myArray);
+        for (int i = 0; i <= halfOfArray; i++) {
+            System.out.print(myArray[i] + " ");
+        }
+
+    }
+
+}
+
